3594e5ae8ed8179a73d9ce4b0ad91386c3931455
Rohit <rohitkumar.0667@gmail.com>
2020-06-05 16:11:08 +0530

    Routing with more than one callback function

--- basicexpress/index.js
+++ basicexpress/index.js
@@ -27,6 +27,69 @@ app.get("/users/:id?", (req, res) => {
 
 });
 
+app.get('/flight/:from?.:to?', (req, res) => {
+    if (req.params.from == undefined) {
+        res.send("Welcome to flight ");
+    } else {
+        res.send(`Welcome to user you are travel from ${req.params.from} to ${req.params.to}`);
+    }
+});
+
+var cb0 = function (req, res, next) {
+    console.log('CB0')
+    next()
+}
+
+var cb1 = function (req, res, next) {
+    console.log('CB1')
+    next()
+}
+
+var cb2 = function (req, res) {
+    res.send('Hello from C!')
+}
+
+app.get('/example/c', [cb0, cb1, cb2]);
+
+app.get('/example/a', function (req, res) {
+    res.send('Hello from A!')
+});
+
+app.get('/example/b', function (req, res, next) {
+    console.log('the response will be sent by the next function ...')
+    next()
+}, function (req, res) {
+    res.send('Hello from B!')
+});
+
+var cb0 = function (req, res, next) {
+    console.log('CB0')
+    next()
+}
+
+var cb1 = function (req, res, next) {
+    console.log('CB1')
+    next()
+}
+
+app.get('/example/d', [cb0, cb1], function (req, res, next) {
+    console.log('the response will be sent by the next function ...')
+    next()
+}, function (req, res) {
+    res.send('Hello from D!')
+});
+
+app.route('/book')
+  .get(function (req, res) {
+    res.send('Get a random book')
+  })
+  .post(function (req, res) {
+    res.send('Add a book')
+  })
+  .put(function (req, res) {
+    res.send('Update the book')
+  })
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 
